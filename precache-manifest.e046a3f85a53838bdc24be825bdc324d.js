dc30b470eac205b9d711c39ae3eb15993b7d4129
jh <zh.cecon@gmail.com>
2022-06-03 20:52:06 +0900

    Updates

---
+++ precache-manifest.e046a3f85a53838bdc24be825bdc324d.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = [
+  {
+    "revision": "94e05213a8b50bb1b344",
+    "url": "./static/css/main.fac30ad5.chunk.css"
+  },
+  {
+    "revision": "94e05213a8b50bb1b344",
+    "url": "./static/js/main.94e05213.chunk.js"
+  },
+  {
+    "revision": "402142f5138cc4009625",
+    "url": "./static/css/1.ff3cf8f6.chunk.css"
+  },
+  {
+    "revision": "402142f5138cc4009625",
+    "url": "./static/js/1.402142f5.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "688ba966bf9ad4cba89b8d3ad8b79905",
+    "url": "./static/media/ico-x.688ba966.svg"
+  },
+  {
+    "revision": "caf3bb80e2e8dcd77a75f4c84d7a0645",
+    "url": "./static/media/ico-back_dark.caf3bb80.svg"
+  },
+  {
+    "revision": "4d1085c73e2d8553cc781f4e751a2696",
+    "url": "./static/media/ico-bonoland.4d1085c7.svg"
+  },
+  {
+    "revision": "a86e45fe2aaa349205807d590d8d5f6a",
+    "url": "./static/media/ico-side_left.a86e45fe.svg"
+  },
+  {
+    "revision": "ac6476deb211addf0161164a764c2640",
+    "url": "./static/media/ico-back_white.ac6476de.svg"
+  },
+  {
+    "revision": "9791ba7fd6127a288b2d0b3dd0287bea",
+    "url": "./static/media/ico-nursing.9791ba7f.svg"
+  },
+  {
+    "revision": "4c0053ce6e08e2f4c75892f66972b391",
+    "url": "./static/media/ico-house.4c0053ce.svg"
+  },
+  {
+    "revision": "d5a3dcf544ec82da843e0dacebdcfbd2",
+    "url": "./static/media/ico-town.d5a3dcf5.svg"
+  },
+  {
+    "revision": "f6ea8ff420ebc9ed4237d4e18afc7ce8",
+    "url": "./static/media/ico-rising.f6ea8ff4.svg"
+  },
+  {
+    "revision": "3cfc5a747d910d0c0836410a2cce5507",
+    "url": "./static/media/ico-traffic.3cfc5a74.svg"
+  },
+  {
+    "revision": "77e23a62934d4d0e3c1cb98e8d2bb030",
+    "url": "./static/media/ico-market.77e23a62.svg"
+  },
+  {
+    "revision": "c07708f2c459254aefe60b9cbf67bfb0",
+    "url": "./static/media/ico-road.c07708f2.svg"
+  },
+  {
+    "revision": "25e0b4a9fdd0d9108a11577283a55d9e",
+    "url": "./static/media/ico-calculator.25e0b4a9.svg"
+  },
+  {
+    "revision": "adabc5e04c2ba3cd174ef16f08028a90",
+    "url": "./static/media/ico-blocks.adabc5e0.svg"
+  },
+  {
+    "revision": "a26cfe231742dd49bd5feb8bcaa66f18",
+    "url": "./static/media/ico-thumbup.a26cfe23.svg"
+  },
+  {
+    "revision": "9e6984b71e88a209cce97ae11680c9d5",
+    "url": "./static/media/ico-docs.9e6984b7.svg"
+  },
+  {
+    "revision": "7142a1e6b72e6972d2fc6dc1ca37e399",
+    "url": "./static/media/ico-chat.7142a1e6.svg"
+  },
+  {
+    "revision": "2c8081a034f67edc5d6d8e80ba619f53",
+    "url": "./static/media/ico-user.2c8081a0.svg"
+  },
+  {
+    "revision": "7e7c25e81561d4be15f43c3b797ce839",
+    "url": "./static/media/ico-man.7e7c25e8.svg"
+  },
+  {
+    "revision": "7256eba4af8151ffda66e8c525cfd4d4",
+    "url": "./static/media/ico-bell.7256eba4.svg"
+  },
+  {
+    "revision": "d26c50d9df365cbe2e24d8d27e5ac9a7",
+    "url": "./static/media/ico-cadastral.d26c50d9.svg"
+  },
+  {
+    "revision": "49e9940e36ceb47fc8dbde7ab8e898ac",
+    "url": "./static/media/ico-location.49e9940e.svg"
+  },
+  {
+    "revision": "0882acccd0fb8c0e87d8479abf950f2b",
+    "url": "./static/media/ico-checkbox02_default.0882accc.svg"
+  },
+  {
+    "revision": "d77d3fd5597a48c4a1c36267cd1f41c3",
+    "url": "./static/media/ico-checkbox02_checked.d77d3fd5.svg"
+  },
+  {
+    "revision": "7431d487081ca973d551e4a81426a8f9",
+    "url": "./static/media/ico-search.7431d487.svg"
+  },
+  {
+    "revision": "fd7f5cd01fbf6aa67da934ffbda6ce2c",
+    "url": "./static/media/ico-symbol.fd7f5cd0.svg"
+  },
+  {
+    "revision": "4597b25c4c409e626780d6fa260e8f2b",
+    "url": "./static/media/ico-reset.4597b25c.svg"
+  },
+  {
+    "revision": "7e57c8425ea7853623cc1a0b9a3bda6c",
+    "url": "./static/media/ico-slider.7e57c842.svg"
+  },
+  {
+    "revision": "fd2466cbd4ec189e373ebdc86d094112",
+    "url": "./static/media/ico-star.fd2466cb.svg"
+  },
+  {
+    "revision": "f2ed6af437fb82da481dd54c654cb052",
+    "url": "./static/media/ico-share.f2ed6af4.svg"
+  },
+  {
+    "revision": "dafa7ebfedb077d91a3c025b43073c6c",
+    "url": "./static/media/card-thumbnail.dafa7ebf.png"
+  },
+  {
+    "revision": "8f0c51ac4cf22f2af140fe7cd0ab0e6c",
+    "url": "./static/media/ico-back.8f0c51ac.svg"
+  },
+  {
+    "revision": "f43fe8a5454debd752fb1edc0a2763e0",
+    "url": "./static/media/ico-accordion.f43fe8a5.svg"
+  },
+  {
+    "revision": "ae5b07f10713f5db19d4d64d0aedef20",
+    "url": "./static/media/ico-sync.ae5b07f1.svg"
+  },
+  {
+    "revision": "27f718dfa8ff018156448fa1ca0a423c",
+    "url": "./static/media/item-thumbnail.27f718df.png"
+  },
+  {
+    "revision": "fb6f6a929711be00edfc08166f6ae60c",
+    "url": "./static/media/ico-x_white.fb6f6a92.svg"
+  },
+  {
+    "revision": "8f0c51ac4cf22f2af140fe7cd0ab0e6c",
+    "url": "./static/media/ico-arrowback_white.8f0c51ac.svg"
+  },
+  {
+    "revision": "bd14013ca652d15d0f7964d14a207872",
+    "url": "./static/media/ico-checkbox_default.bd14013c.svg"
+  },
+  {
+    "revision": "bb7504bf54f8a6c3f7a0b55dd43cf304",
+    "url": "./static/media/ico-checkbox_checked.bb7504bf.svg"
+  },
+  {
+    "revision": "50bd03399ad12605a4d662057ee072b6",
+    "url": "./static/media/ico-accordion_black.50bd0339.svg"
+  },
+  {
+    "revision": "1e633579041fafd5870be95f888efa37",
+    "url": "./static/media/ico-infowindow.1e633579.svg"
+  },
+  {
+    "revision": "472930af728c87a5c87acef385a1ad1d",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8d20df87591d4182379ddbc7e688331.js"
+  "./precache-manifest.e046a3f85a53838bdc24be825bdc324d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
